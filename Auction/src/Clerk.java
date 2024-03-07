aba6c28fc0d3f891823f294194356e17a5526eaf
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-03-07 12:25:14 +0100

    Clerk

---
+++ Auction/src/Clerk.java
@@ -0,0 +1,14 @@
+public class Clerk {
+
+private Item item;
+private Auction auction;
+
+    Clerk(){}
+    public  createAuction(){}
+    public  startAuction(){}
+    public String stopAuction(){
+        return getBidder();
+    }
+    public void addItem(String item){}
+    public getCurrentBid(){}
+}
